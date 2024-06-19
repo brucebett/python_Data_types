e8efd8a9cb2089d5f1d8f368ad14a9e7b4142916
brucebett <bett6448@gmail.com>
2024-06-19 10:24:42 +0300

    fisrt commit

---
+++ index.py
@@ -0,0 +1,66 @@
+print("")
+print("Hello World")
+print("Introduction to python")
+print(200)
+print("brian")
+print("100.5")
+print(100.5)
+
+age = 20
+name = "George"
+last_name = "kinuthia"
+height = 6.2
+print(age)
+print(name)
+print(last_name)
+print(height)
+
+# DATA TYPES
+# Integer
+marks = 100
+print(marks)
+# String
+d = "this is my first string"
+print(d)
+print("this is my first string")
+print(500)
+f = "267"
+p = "these are andrews favorite color"
+
+# Boolean
+is_present = True
+is_boy = False
+# Float
+x = 24.576
+print(24.576)
+# Input from user
+first_name = input("what is your name?")
+print(first_name)
+color = input("what is your favorite color?")
+print(color)
+pet = input("what is your favorite pet?")
+print(pet)
+year_of_birth = input("what is your year of birth?")
+print(year_of_birth)
+
+# Concatenation
+position = input("what is your position?")
+print("your are position " + position)
+family_name = input("what is your family name?")
+print("Hello " + family_name)
+print("Goodmorning " + family_name)
+# details = input("what are your names")
+# print("Hello " + details)
+
+
+greeting = "hello"
+name = "peter njuguna"
+role = "you are a proffesor"
+
+full_sentence = f"{greeting} {name} {role}"
+print(full_sentence)
+
+first_name = input("enter your first name")
+last_name = input("enter your last name")
+profession = input("enter your profession")
+input("Hello " + first_name + " " + last_name + " " + "you are a  " + profession)
